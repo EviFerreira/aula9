d3045a36accc9f8f2c94fa10b4bdce540d6ea51e
EviFerreira <164196834+EviFerreira@users.noreply.github.com>
2024-04-10 11:28:30 -0300

    Add files via upload

---
+++ aula9.js
@@ -0,0 +1,28 @@
+function somar() {
+    var num1 = document.getElementById
+}
+
+function somar() {
+    var num1 = document.getElementById("n1").value;
+    var num2 = document.getElementById("n2").value;
+    var  resultado = document.getElementById("res");
+    num1 = Number (num1)
+    num2 = Number (num2)
+    resu = num1 + num2
+    resultado.innerHTML = (`o resultado da soma entre ${num1} + ${num2} e igual a: ${resu}`)
+}
+{
+var nome = window.prompt (`Qual seu nome? `)//var guardar o nome
+var nome = document.write(`Olá ${nome}.seu nome tem ${nome.length} letras <br> `);
+// //document.write o placeholder ser em crase
+var nome = document.write(`seu nome em maiusculo é ${nome.toLocaleLowerCase()}<br>`);
+var nome = document.write(`seu nome em menusculo é ${nome.toLocaleLowerCase()}<br>`);
+
+// organizar numéros
+ var n1 = 2310.8
+ n1.toFixed(2) //  quantidade de numero apos a virgula
+ n1.toFixed(2).replace (',' , ',') // replece substitui o ponto por virgula
+ //n1.toLocalstring(pt-BR).(style.currenty)
+ windons alert (n1)
+
+}
